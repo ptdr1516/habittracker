e23b793e481137e2678e085d922f986e9be68280
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-20 22:22:04 +0530

    FEAT: handling month dates

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+const Habits = require('../models/Habits');
+
+router.get('/', (req, res) => {
+    res.render('homepage')
+});
+
+// Handling Month Dates
+function getDate(x) {
+    let d = new Date();
+    d.setDate(d.getDate() + x);
+
+    var newDate =
+        d.toLocaleDateString('pt-br')
+            .split('/')
+            .reverse()
+            .join('-');
+
+    var day;
+
+    switch (d.getDay()) {
+        case 0: day = 'Sun'; break;
+        case 1: day = 'Mon'; break;
+        case 2: day = 'Tue'; break;
+        case 3: day = 'Wed'; break;
+        case 4: day = 'Thu'; break;
+        case 5: day = 'Fri'; break;
+        case 6: day = 'Sat'; break;
+    }
+
+    return {
+        date: newDate(),
+        day
+    };
+}
+
+module.exports = router;
