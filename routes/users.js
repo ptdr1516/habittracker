95fb9807dd25b65bead3e980bb3ed4c86e4fe9d9
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-20 22:37:56 +0530

    FEAT: handling new user registration

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+// Login Page
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+// Registration Page
+router.get('/register', (req, res) => {
+   res.render('register')
+});
+
+// Feature Registration
+router.post('/register', (req, res) => {
+    const { name, email } = req.body;
+
+    let errors = [];
+
+    if (!name || !email) {
+        errors.push({
+            msg: 'All fields must be provided'
+        });
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email
+        });
+    }
+    else {
+        User.findOne({ email: email }).then(user => {
+            if (user) {
+                // User already exists
+                errors.push({
+                    msg: 'Email id already exists'
+                });
+                res.render('register', {
+                    errors,
+                    name,
+                    email
+                });
+            }
+            else {
+                const newUser = new User({
+                    name, email
+                });
+
+                newUser.save().then((user) => {
+                    req.flash('successMsg','Your account has been registered');
+                    res.redirect('/users/login');
+                })
+                    .catch((err) => {
+                        console.log('Error',err);
+                    })
+
+            }
+        });
+    }
+});
